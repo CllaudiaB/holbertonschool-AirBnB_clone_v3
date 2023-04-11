4fdcdbc793483133c2b05d6519f789c69b60f790
annlaure02 <alaure.g02@gmail.com>
2023-04-11 21:18:01 +0200

    added import in init file fixed pep8 for other files

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,7 +6,7 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+@app_views.route('/status')
 def views_json():
     """ returns a JSON: "status": "OK" """
     return jsonify({'status': 'OK'})
