bdf449664c44b740e5d1f95414c1596e0835278e
root <root@Claudia.localdomain>
2023-04-13 13:53:17 +0200

    added file amenity

---
+++ api/v1/views/amenities.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""Create a new view for Amenity objects that handles all
+   default RESTFul API actions"""
+from models import storage
+from models import base_model
+from api.v1.views import app_views
+from models.amenity import Amenity
+from flask import jsonify, abort, request
+
+
+@app_views.route('/amenities', methods=['GET'], strict_slashes=False)
+def amenities():
+    """Retrieves the list of all Amenity objects"""
+    all_amenities = []
+    dict_amenities = storage.all(Amenity).values()
+    for amenity in dict_amenities:
+        all_amenities.append(amenity.to_dict())
+    return jsonify(all_amenities)
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['GET'], strict_slashes=False)
+def error_amenity(amenity_id):
+    """ Retrieves a Amenity object """
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+    return jsonify(amenity.to_dict())
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_amenity(amenity_id):
+    """delete Amenity"""
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+    amenity.delete()
+    storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route('/amenities', methods=['POST'], strict_slashes=False)
+def create_amenity():
+    """create amenity"""
+    json_data = request.get_json()
+    if json_data is None:
+        abort(400, 'Not a JSON')
+    if json_data.get('name') is None:
+        abort(400, 'Missing name')
+    amenity = Amenity(**json_data)
+    amenity.save()
+    return jsonify(amenity.to_dict()), 201
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['PUT'], strict_slashes=False)
+def update_amenity(amenity_id):
+    """ Updates a Amenity object """
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+    json_data = request.get_json()
+    if json_data is None:
+        abort(400, 'Not a JSON')
+    for key, value in json_data.items():
+        if key not in ['id', 'created_at', 'updated_at']:
+            setattr(amenity, key, value)
+    amenity.save()
+    return jsonify(amenity.to_dict()), 200
