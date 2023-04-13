af7219d49ad4b83e4a47c91e36d689a5648f4f60
root <root@Claudia.localdomain>
2023-04-13 13:58:57 +0200

    fixed pep8

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -18,7 +18,8 @@ def amenities():
     return jsonify(all_amenities)
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>', methods=['GET'],
+                 strict_slashes=False)
 def error_amenity(amenity_id):
     """ Retrieves a Amenity object """
     amenity = storage.get(Amenity, amenity_id)
@@ -52,7 +53,8 @@ def create_amenity():
     return jsonify(amenity.to_dict()), 201
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>', methods=['PUT'],
+                 strict_slashes=False)
 def update_amenity(amenity_id):
     """ Updates a Amenity object """
     amenity = storage.get(Amenity, amenity_id)
